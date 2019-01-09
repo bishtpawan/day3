68af6861e6058bb73f3cc781fe67beb0b17307ae
bishtpawan <pawan.knoldus@knoldus.in>
2019-01-09 12:03:24 +0530

    owership implemenation

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+  let s1 = String::from("hello");
+  let (s2, len) = get_length(s1);
+  println!("The length of {} is {}",s2 ,len);
+
+}
+
+fn get_length(s:String)->(String,usize){
+  let length = s.len();
+  (s,length)
+
+}
