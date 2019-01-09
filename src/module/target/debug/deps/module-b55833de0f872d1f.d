2f4e8e93e8453a905fe8dcf650dfe349e2da8b6a
bishtpawan <pawan.knoldus@knoldus.in>
2019-01-09 22:24:58 +0530

    understanding of module

---
+++ src/module/target/debug/deps/module-b55833de0f872d1f.d
@@ -0,0 +1,5 @@
+/home/knoldus/IdeaProjects/Day3/src/module/target/debug/deps/module-b55833de0f872d1f: src/main.rs
+
+/home/knoldus/IdeaProjects/Day3/src/module/target/debug/deps/module-b55833de0f872d1f.d: src/main.rs
+
+src/main.rs:
---
+++ src/module/target/debug/module.d
@@ -0,0 +1,1 @@
+/home/knoldus/IdeaProjects/Day3/src/module/target/debug/module: /home/knoldus/IdeaProjects/Day3/src/module/src/main.rs
